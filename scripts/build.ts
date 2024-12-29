41c56a50d0c8ba1832deb8e7cec78e9d2222aa7b
exidna <dm.a.titov@ya.ru>
2024-12-29 22:09:11 +0300

    feat: bundle index.html

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,9 +1,14 @@
 import * as esbuild from "https://deno.land/x/esbuild@v0.24.2/mod.js";
+import htmlPlugin from "npm:@chialab/esbuild-plugin-html";
 
 await esbuild.build({
-    entryPoints: ["src/main.ts"],
-    outdir: "build",
-    bundle: true,
+  entryPoints: [
+    "src/main.ts",
+    "src/index.html",
+  ],
+  outdir: "build",
+  bundle: true,
+  plugins: [htmlPlugin()],
 });
 
 await esbuild.stop();
