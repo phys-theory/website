94e281fe628f747e35483523a5968679b1ca14f7
exidna <dm.a.titov@ya.ru>
2024-12-30 14:34:25 +0300

    refactor: cleanup

--- src/fresh.config.ts
+++ src/fresh.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
 
 export default defineConfig({
+  build: {
+    outDir: "../build",
+  },
+  staticDir: "static",
   plugins: [tailwind()],
 });
